3d41f556fe6a9b3e02ea3d61fa7180f404d1dc16
Avsecz <zigaavsec@gmail.com>
2017-09-13 18:35:42 +0200

    feat: implemented git-lfs model pulling
    TODO - write unit tests and setup travis

--- tests/test_0_config.py
+++ tests/test_0_config.py
@@ -12,3 +12,10 @@ def test_config_file_exists():
 def test_load_config():
     assert modelzoo.config.kipoi_models_repo() == os.path.join(os.path.expanduser('~'), ".kipoi/models/")
     assert modelzoo.config.other_models_repo() == []
+
+
+def test_load_models():
+    # TODO - setup git-lfs in travis
+    # TODO - test: pull_kipoi_models
+    # TODO - test: pull_kipoi_model
+    pass
