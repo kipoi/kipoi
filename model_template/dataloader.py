69f9cb6ec8d373271eb703273e689ab0a6d56e87
Avsecz <zigaavsec@gmail.com>
2017-10-30 15:50:03 +0100

    chore: writing the ipynb on model contribution

--- model_template/dataloader.py
+++ model_template/dataloader.py
@@ -1,2 +1,43 @@
 """Dataloader
 """
+from kipoi.data import Dataset
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelBinarizer
+
+
+class MyDataset(Dataset):
+
+    def __init__(self, features_file, targets_file=None):
+        """
+
+        """
+        self.features_file = features_file
+        self.targets_file = targets_file
+
+        self.features = pd.read_csv(features_file)
+        if targets_file is not None:
+            self.targets = pd.read_csv(targets_file)
+            assert len(self.targets) == len(self.features)
+
+    def __len__(self):
+        return len(self.features)
+
+    def __getitem__(self, idx):
+        out = {}
+        # inputs
+        out["inputs"] = {
+            "features": self.features.iloc[idx].values
+        }
+
+        # optional targets
+        if self.targets_file is not None:
+            out["targets"] = {
+                "plant_class": self.targets.iloc[idx].values
+            }
+
+        # metadata
+        out["metadata"] = {
+            "example_row_number": np.array([idx])
+        }
+        return out
