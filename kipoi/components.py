d72cd767b5ac29b36970e7a86f38e9fe32cc8870
Žiga Avsec <Avsecz@users.noreply.github.com>
2018-03-02 21:59:26 +0100

    feat: softened the requirements. doc field not allways mandatory (#168)

--- kipoi/components.py
+++ kipoi/components.py
@@ -47,11 +47,15 @@ class Info(RelatedConfigMixin):
       version: 0.1
     """
     authors = related.SequenceField(Author, repr=True)
-    doc = related.StringField()  # free-text description of the model
+    doc = related.StringField("", required=False)  # free-text description of the model
     name = related.StringField(required=False)  # TODO - deprecate
     version = related.StringField(default="0.1", required=False)
     license = related.StringField(default="MIT", required=False)  # license of the model/dataloader - defaults to MIT
     tags = StrSequenceField(str, default=[], required=False)
+
+    def __attrs_post_init__(self):
+        if self.doc == "":
+            logger.warn("doc empty for the `info:` field")
 
 
 @related.immutable(strict=True)
@@ -85,7 +89,7 @@ class ArraySchema(RelatedConfigMixin):
     """
     verbose = True
     shape = TupleIntField()
-    doc = related.StringField()
+    doc = related.StringField("", required=False)
     # MAYBE - allow a list of strings?
     #         - could be useful when a single array can have multiple 'attributes'
     name = related.StringField(required=False)
@@ -476,12 +480,16 @@ class PostProcModelStruct(RelatedConfigMixin):
 @related.immutable(strict=True)
 class DataLoaderArgument(RelatedConfigMixin):
     # MAYBE - make this a general argument class
-    doc = related.StringField()
+    doc = related.StringField("", required=False)
     example = AnyField(required=False)
     name = related.StringField(required=False)
     type = related.StringField(default='str', required=False)
     optional = related.BooleanField(default=False, required=False)
     tags = StrSequenceField(str, default=[], required=False)  # TODO - restrict the tags
+
+    def __attrs_post_init__(self):
+        if self.doc == "":
+            logger.warn("doc empty for one of the dataloader `args` fields")
 
 
 @related.immutable(strict=True)
--- tests/test_022_parsing_ArraySchema.py
+++ tests/test_022_parsing_ArraySchema.py
@@ -12,10 +12,10 @@ shape: (100, )
 doc: some input
 """, """
 shape: (None, 100)
-doc: some input
+# doc: some input
 """, """
 shape: (None, )
-doc: some input
+# doc: some input
 """, """
 shape: (100, )
 doc: some input
@@ -32,9 +32,6 @@ associated_metadata: ranges  # as a single element
 """]
 
 BAD_EXAMPLES = ["""
-shape: (100, )
-# doc missing
-""", """
 # shape missing
 doc: some input
 """, """
@@ -50,26 +47,26 @@ GOOD_EXAMPLES_COLNAMES = [("""
 shape: (1, )
 doc: Predicted binding strength
 column_labels:
-    - rbp_prb""",["rbp_prb"]),("""
+    - rbp_prb""", ["rbp_prb"]), ("""
 shape: (3, )
 doc: Predicted binding strength
 column_labels:
-    - tests/test_model_column_names.txt""",['First', 'Second', 'Third']),("""
+    - tests/test_model_column_names.txt""", ['First', 'Second', 'Third']), ("""
 shape: (1, )
 doc: Predicted binding strength
-column_labels: rbp_prb""",["rbp_prb"])
+column_labels: rbp_prb""", ["rbp_prb"])
 ]
 
 BAD_EXAMPLES_COLNAMES = ["""
 shape: (3, )
 doc: Predicted binding strength
 column_labels:
-    - rbp_prb""","""
+    - rbp_prb""", """
 shape: (1, )
 doc: Predicted binding strength
 column_labels:
     - tests/test_model_column_names.txt"""
-]
+                         ]
 
 
 @pytest.mark.parametrize("info_str", GOOD_EXAMPLES)
@@ -91,7 +88,7 @@ def test_parse_bad_info(info_str):
         CLS.from_config(bim)
 
 @pytest.mark.parametrize("info_str,info_res", GOOD_EXAMPLES_COLNAMES)
-def test_parse_col_naming(info_str,info_res):
+def test_parse_col_naming(info_str, info_res):
     bim = from_yaml(info_str)
     info2 = CLS.from_config(bim)
     assert(info2.column_labels == info_res)
