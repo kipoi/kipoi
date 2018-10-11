f966a8cf4fb09783c18b167ff918b10163603e72
Žiga Avsec <Avsecz@users.noreply.github.com>
2018-10-11 12:11:34 +0200

    Unify loading (#346)
    * allow type as default
    
    * add default flag to dataloader args
    
    * sys.modules import in the right order
    
    * fix load_obj - prevent removing objects
    
    * fix exception raising
    
    * fix nested imports
    
    * add also a unified model loading

--- example/models/kipoi_dataloader_decorator/dataloader.py
+++ example/models/kipoi_dataloader_decorator/dataloader.py
@@ -38,6 +38,16 @@ class MyDataset(Dataset):
               url: https://github.com/kipoi/kipoi/raw/57734d716b8dedaffe460855e7cfe8f37ec2d48d/example/models/sklearn_iris/example_files/targets.csv
               md5: 54e058d31d05897836302cd6961212a1
             optional: True
+        x_transformer:
+            doc: input_transformer
+            default:
+              url: https://github.com/kipoi/kipoi/raw/57734d716b8dedaffe460855e7cfe8f37ec2d48d/example/models/sklearn_iris/dataloader_files/x_transformer.pkl
+              md5: bc1bf3c61c418b2d07506a7d0521a893
+        y_transformer:
+            doc: input_transformer
+            default:
+              url: https://github.com/kipoi/kipoi/raw/57734d716b8dedaffe460855e7cfe8f37ec2d48d/example/models/sklearn_iris/dataloader_files/y_transformer.pkl
+              md5: 7332d50fd461c49ac6ce24c194abbbd3
         dummy:
             doc: dummy argument
             example: 5
@@ -66,17 +76,15 @@ class MyDataset(Dataset):
                 doc: Just an example metadata column
     """
 
-    def __init__(self, features_file, targets_file=None, dummy=None):
+    def __init__(self, features_file, targets_file=None, x_transformer=None, y_transformer=None, dummy=None):
         self.features_file = features_file
         self.targets_file = targets_file
         self.dummy = dummy
 
-        base_url = "https://github.com/kipoi/kipoi/raw/57734d716b8dedaffe460855e7cfe8f37ec2d48d/example/models/sklearn_iris/dataloader_files"
-        outdir = os.path.abspath(os.path.join(this_path, "downloaded/dataloader_files"))
-        if not os.path.exists(outdir):
-            os.makedirs(outdir)
-        self.y_transformer = read_pickle(get_file(os.path.join(outdir, "y_transformer.pkl"), origin=base_url + '/y_transformer.pkl'))
-        self.x_transformer = read_pickle(get_file(os.path.join(outdir, "x_transformer.pkl"), origin=base_url + '/x_transformer.pkl'))
+        assert x_transformer is not None
+        self.x_transformer = read_pickle(x_transformer)
+        assert y_transformer is not None
+        self.y_transformer = read_pickle(y_transformer)
 
         self.features = pd.read_csv(features_file)
         if targets_file is not None:
