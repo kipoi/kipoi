02a343971f5527113238f85223548e88d48877f4
Avsecz <zigaavsec@gmail.com>
2018-10-19 19:55:30 +0200

    Bump version: 0.5.1 → 0.5.2

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ test_requirements = [
 
 setup(
     name='kipoi',
-    version='0.5.1',
+    version='0.5.2',
     description="Kipoi: model zoo for genomics",
     author="Kipoi team",
     author_email='avsec@in.tum.de',
