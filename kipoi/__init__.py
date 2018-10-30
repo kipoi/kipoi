16d653eaf5703aa5b7419719067bb30a9e0fdab7
Avsecz <zigaavsec@gmail.com>
2018-10-30 23:48:24 +0100

    Bump version: 0.5.6 → 0.5.7

--- kipoi/__init__.py
+++ kipoi/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 __author__ = 'Kipoi team'
 __email__ = 'avsec@in.tum.de'
-__version__ = '0.5.6'
+__version__ = '0.5.7'
 
 
 # available modules
--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ test_requirements = [
 
 setup(
     name='kipoi',
-    version='0.5.6',
+    version='0.5.7',
     description="Kipoi: model zoo for genomics",
     author="Kipoi team",
     author_email='avsec@in.tum.de',
