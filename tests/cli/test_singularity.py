de05db988affb3efab64027bb31b13450ac9e3f1
DerThorsten <derthorstenbeier@gmail.com>
2019-07-08 10:32:09 +0200

    re-enabling tests

--- tests/cli/test_singularity.py
+++ tests/cli/test_singularity.py
@@ -7,7 +7,7 @@ from kipoi.cli.singularity import (singularity_pull, singularity_exec,
                                    involved_directories,
                                    singularity_command)
 
-@pytest.mark.skip(reason="no singularity tests atm")
+#@pytest.mark.skip(reason="no singularity tests atm")
 def test_singularity_pull_run(tmpdir):
     tmpdir = '/tmp/singularity'
     output_file = os.path.join(str(tmpdir), "hello-world_latest.sif")
@@ -17,6 +17,6 @@ def test_singularity_pull_run(tmpdir):
     # singularity_exec(output_file, ['echo', 'hello-world'])
 
 
-@pytest.mark.skip(reason="circle-ci fails for unknown reasons on this test")
+#@pytest.mark.skip(reason="circle-ci fails for unknown reasons on this test")
 def test_singularity_command_dry_run():
     singularity_command(['kipoi', 'test', 'Basset', '--source=kipoi'], 'Basset', {}, dry_run=True)
