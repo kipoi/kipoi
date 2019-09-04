7110ea9f157c6996197bc75d5c2516d33a9e2861
DerThorsten <derthorstenbeier@gmail.com>
2019-09-04 14:11:37 +0200

    Bump version: 0.6.16 → 0.6.17

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ test_requirements = [
 
 setup(
     name='kipoi',
-    version='0.6.16',
+    version='0.6.17',
     description="Kipoi: model zoo for genomics",
     author="Kipoi team",
     author_email='avsec@in.tum.de',
