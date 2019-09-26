991fa7222e2c9dc139742216e9fe2d2b3908681d
Florian R. Hölzlwimmer <git.ich@frhoelzlwimmer.de>
2019-09-26 12:15:35 +0200

    Bump version: 0.6.16 → 0.6.16

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ class VerifyVersionCommand(install):
 
 
 # this will be overwritten by bumpversion
-version = '0.6.16'
+version = 'version='0.6.16''
 # append git commit hash if version is not tagged
 version = git_version(version)
 
