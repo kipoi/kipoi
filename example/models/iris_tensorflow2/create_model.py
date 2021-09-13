d42854976beb1aa67b92bce52cb113a79ac3e0f5
Haimasree <haimasree.de@gmail.com>
2021-09-13 20:15:31 +0200

    adding create_model

---
+++ example/models/iris_tensorflow2/create_model.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import numpy as np
+
+class BasicModel(tf.train.Checkpoint):
+  def __init__(self):
+    super().__init__()
+    self.bias = tf.Variable(1.)
+    self.weight = tf.Variable(tf.random.normal((4, 3))) 
+
+  @tf.function(input_signature=[tf.TensorSpec(shape=None, dtype=tf.float32)])
+  def __call__(self, inputs):
+    logits = tf.identity(tf.matmul(inputs, self.weight) + self.bias)
+    print(self.weight.shape)
+    y_pred = tf.nn.softmax(logits, name="probas")
+    return y_pred
+
+if __name__ == "__main__":
+    model = BasicModel()
+    tf.saved_model.save(model, "basic_model")
+    reconstructed_model = tf.saved_model.load("basic_model")
+    test_input = np.ones((3, 4))
+    np.testing.assert_allclose(model(test_input), reconstructed_model(test_input))
