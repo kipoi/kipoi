03fc96745e8a07cb35af1e60c5b883e3aa3d5999
Haimasree <haimasree.de@gmail.com>
2021-11-12 12:32:32 +0100

    Adding singularity test back

--- tests/cli/test_singularity.py
+++ tests/cli/test_singularity.py
@@ -7,16 +7,5 @@ from kipoi.cli.singularity import (singularity_pull, singularity_exec,
                                    involved_directories,
                                    singularity_command)
 
-@pytest.mark.skip(reason="no singularity tests atm")
-def test_singularity_pull_run(tmpdir):
-    tmpdir = '/tmp/singularity'
-    output_file = os.path.join(str(tmpdir), "hello-world_latest.sif")
-
-    singularity_pull("shub://vsoch/hello-world:latest", output_file)
-    # For now, don't run the command
-    # singularity_exec(output_file, ['echo', 'hello-world'])
-
-
-@pytest.mark.skip(reason="circle-ci fails for unknown reasons on this test")
 def test_singularity_command_dry_run():
     singularity_command(['kipoi', 'test', 'Basset', '--source=kipoi'], 'Basset', {}, dry_run=True)
