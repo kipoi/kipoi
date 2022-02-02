9b01c6c718c71de782069fe5be3cfdfdba35d121
Haimasree <haimasree.de@gmail.com>
2022-02-02 12:48:47 +0100

    basic tests added

--- tests/test_17_modeldescriptionclass.py
+++ tests/test_17_modeldescriptionclass.py
@@ -1,6 +1,9 @@
+import pytest
+
 from kipoi.kipoimodeldescription import KipoiModelDescription, KipoiModelSchema, Dependencies, KipoiModelTest
 
-def test_basic_modeldescription_class():
+@pytest.fixture
+def model_parameters():
     args = {
         'weights': {
             'md5': '4878981d84499eb575abd0f3b45570d3',
@@ -39,5 +42,22 @@ def test_basic_modeldescription_class():
             'md5': '1adb12be84240ffb7d7ca556eeb19e01'
             }
     )
-    mdc = KipoiModelDescription(args=args, schema=schema, defined_as='model.APARENTModel', dependencies=dependencies, model_test=model_test)
+    return (args, schema, dependencies, model_test)
+
+def test_modeldescription_missing_modeltype(model_parameters):
+    with pytest.raises(ValueError):
+        mdc = KipoiModelDescription(args=model_parameters[0], schema=model_parameters[1], 
+                            defined_as='', dependencies=model_parameters[2], 
+                            model_test=model_parameters[3])
+
+
+def test_basic_modeldescription_class(model_parameters):
+    mdc = KipoiModelDescription(args=model_parameters[0], schema=model_parameters[1], 
+                            defined_as='model.APARENTModel', dependencies=model_parameters[2], 
+                            model_test=model_parameters[3])
     assert mdc.args['weights']['md5'] == '4878981d84499eb575abd0f3b45570d3'
+    assert mdc.schema.inputs['shape'][0] == 205
+    assert mdc.schema.targets['distal_prop']['shape'][0] == 1
+    assert mdc.dependencies.conda== ['python=3.9', 'tensorflow', 'keras>=2.0.4,<3']
+    assert mdc.dependencies.pip == []
+    assert mdc.model_test.expect['md5'] == '1adb12be84240ffb7d7ca556eeb19e01'
