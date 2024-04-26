72647766eefc9a999067cf7e10f0ed9fd591dafb
Florian R. Hölzlwimmer <Hoeze@users.noreply.github.com>
2024-04-26 17:57:13 +0200

    Update test_RemoteFile.py

--- tests/specs/test_RemoteFile.py
+++ tests/specs/test_RemoteFile.py
@@ -4,7 +4,13 @@ from kipoi.specs import RemoteFile
 
 
 def test_RemoteFile(tmpdir):
-    f = RemoteFile("https://raw.githubusercontent.com/kipoi/kipoi/d35d30b10dd25cc5f6d85de48cb201f3c01d4b3e/README.md", "2ff2e3f12d0986fb7e32453f8451a3e2")
+    remote_url = "https://raw.githubusercontent.com/kipoi/kipoi/d35d30b10dd25cc5f6d85de48cb201f3c01d4b3e/README.md"
+    remote_mdf5sum = "2ff2e3f12d0986fb7e32453f8451a3e2"
+    
+    f = RemoteFile(remote_url, remote_mdf5sum)
     p = tmpdir.mkdir("sub").join("output")
     f.get_file(str(p))
+    assert f.validate(str(p)) is True
+    
+    f = RemoteFile(remote_url, "some_wrong_md5_hash")
     assert f.validate(str(p)) is False
